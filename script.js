53d4ddcb354bfe8a0eb5c2b818767c0b58cb246f
haxwins <haxregist@gmail.com>
2018-01-30 00:06:54 +0100

    alpha 0.5

---
+++ script.js
@@ -0,0 +1,52 @@
+window.addEventListener('load', ()=>{
+	let currentSite = 1;
+	
+	sendRequest = (currentSite) =>{
+		let url = 'http://jbzdy.pl/strona/' + currentSite;
+		let req = new XMLHttpRequest();
+		req.open("GET", url, true);
+		req.addEventListener("load", () => {
+			if(req.status == 200 && req.readyState == 4){
+				convert(req.responseText);
+			}
+			else{
+				console.log("errr");
+			}
+		});
+		req.send(null);
+	}
+	
+	convert = (site) =>{
+			let arr = [];
+			site = site.slice(site.indexOf('<div class="image rolled">'), site.length);
+			site = site.slice(0,site.indexOf('<!-- Billboard__SG_bottom -->'));
+			for(let i=0;i<8;i++){
+				let x = site.slice(site.indexOf("<img src="),site.indexOf(" />"));
+				x = x.slice(x.indexOf("https://"),x.indexOf(".jpg")+4);
+				arr[i]=x;
+				site = site.slice(site.indexOf('<div class="media">'),site.length);
+				site = site.slice(site.indexOf('<div class="image rolled">'),site.length);
+			}
+			let allImages = '';
+			for(let i=0;i<8;i++){
+				allImages = allImages + '<img src="' + arr[i] + '"/>';
+			}
+			let imagesElement = document.querySelector(".images");
+			imagesElement.innerHTML = allImages;
+	}
+	sendRequest(currentSite);
+	let btn = document.getElementsByClassName("next-btn")[0];
+	let pbtn = document.getElementsByClassName("prev-btn")[0];
+	prevRequest = () =>{
+		if(currentSite!==1) currentSite-=1;
+		sendRequest(currentSite);
+		window.scrollTo(0,0);
+	}
+	nextRequest = () =>{
+		currentSite+=1;
+		sendRequest(currentSite);
+		window.scrollTo(0,0);
+	}
+	btn.addEventListener('click',nextRequest,false);
+	pbtn.addEventListener('click',prevRequest,false);
+})
